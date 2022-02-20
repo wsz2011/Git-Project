7176185f599ba5a7221b6897c96a50c77d30147e
zhb_golbal <807933196@qq.com>
2022-02-20 23:23:29 +0800

    idea:3 commit

--- src/main/java/com/git/test/demo1.java
+++ src/main/java/com/git/test/demo1.java
@@ -4,6 +4,8 @@ public class demo1 {
     public static void main(String[] args) {
         System.out.println("Idea 2020.3 Git:first test commit ");
         System.out.println("Idea 2020.3 Git:first test commit2 ");
+        System.out.println("Idea 2020.3 Git:first test commit3 ");
+
 
     }
 }
